9de1f52348089bce012b8c5a795a5ee408de86f4
Herb Caudill <herb@devresults.com>
2019-06-27 11:57:13 +0200

    lib/paths:

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,17 +1,18 @@
 import * as path from 'path'
 import { config } from './config'
 
-export const getTestPath = (sourcePath: string, options = config({})): string => {
+export const getTestPath = (sourcePath: string, options = config({})): string | undefined => {
   const { sourceRegex, sourceReplace } = options
   return getMatchingPath(sourcePath, sourceRegex, sourceReplace)
 }
 
-export const getSourcePath = (testPath: string, options = config({})): string => {
+export const getSourcePath = (testPath: string, options = config({})): string | undefined => {
   const { testRegex, testReplace } = options
   return getMatchingPath(testPath, testRegex, testReplace)
 }
 
-const getMatchingPath = (pathA: string, regex: RegExp, replace: string): string => {
+const getMatchingPath = (pathA: string, regex: RegExp, replace: string): string | undefined => {
   pathA = path.normalize(pathA)
-  return path.normalize(pathA.replace(regex, replace))
+  if (pathA.match(regex)) return path.normalize(pathA.replace(regex, replace))
+  else return undefined
 }
