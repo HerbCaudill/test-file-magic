3956e707270f138caf073d1d32096ff11cb9e694
Herb Caudill <herb@devresults.com>
2019-11-05 11:29:32 +0100

    lib/paths.test.ts: comment

--- src/lib/paths.test.ts
+++ src/lib/paths.test.ts
@@ -44,6 +44,7 @@ describe('paths', () => {
   })
 
   describe('tests alongside', () => {
+    // this matches the default, so it's the same same as no config
     const options = config({
       sourceDir: 'src',
       separateTestRoot: false,
@@ -246,7 +247,8 @@ describe('paths', () => {
         ${'_tests\\foo.spec.js'}      | ${undefined}          | ${'wrong test keyword'}
         ${'_tests\\foo.test.js'}      | ${'src\\foo.js'}      | ${''}
         ${'_tests\\foo.test.jsx'}     | ${'src\\foo.jsx'}     | ${''}
-        ${'_tests\\foo.bar.test.js'}  | ${'src\\foo.bar.js'}  | ${''}
+        ${'_tests\\foo.bar.test.js'}  | ${'src\\fo
+        o.bar.js'}  | ${''}
         ${'_tests\\foo\\bar.test.js'} | ${'src\\foo\\bar.js'} | ${''}
         ${'_tests/foo.bar.test.js'}   | ${'src\\foo.bar.js'}  | ${''}
       `('$testPath -> $sourcePath', ({ testPath, sourcePath }) =>
